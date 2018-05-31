dd324a0334a04d3c46f9e55449aa41b5f0def77f
JohnPaulR123 <39170147+JohnPaulR123@users.noreply.github.com>
2018-05-31 13:32:43 -0700

    loop continues

---
+++ Remarkable.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class Remarkable {
+public static void main(String[] args) {
+	String Name = JOptionPane.showInputDialog("What is your name?");
+	if (Name.equals("Tam")) {
+		JOptionPane.showMessageDialog(null, "");
+	}
+	if (Name.equals("Emilse")) {
+		
+	}
+	if (Name.equals("Reyna")) {
+		
+	}
+	if (Name.equals("Emilio")) {
+		
+	}
+}
+	
+	
+}
