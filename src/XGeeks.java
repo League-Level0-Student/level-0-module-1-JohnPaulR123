b2c8527caa4cae2342132d3eb82ed7ad1651af4d
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-06-14 13:23:41 -0700

    Not Finished with XGeeks

--- src/XGeeks.java
+++ src/XGeeks.java
@@ -1,8 +1,6 @@
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
-
-
-
 
 import javax.swing.JOptionPane;
 
@@ -12,14 +10,28 @@ import javax.swing.JOptionPane;
 * your program should say "June's superpower is writing recipes".
 */
 public class XGeeks {
-public static void main(String[] args) {
+	public static void main(String[] args) {
 
-		// 1. Save the superpower for each person in a variable.
+		String Emilse = "Being patient";
+		String Kassandra = "Singing";
+		String Robert = "speed";
+		String johnPaul = "funny";
+		String Emilio = "Flight";
 
-		// 2. Ask the user to enter a name. Store their answer in a variable.
-
-		// 3. Show the superpower in a pop-up, depending on the name entered. 
-
+		String name = JOptionPane.showInputDialog("Enter a name?");
+		if (name.equals("Emilio")) {
+			JOptionPane.showMessageDialog(null, "Emilio's super power is " + Emilio + "!");
 	}
-}
+		if (name.equals("John Paul")) {
+			JOptionPane.showMessageDialog(null, "John Paul's super power is" + johnPaul + "!");
+		}
+		}
+	}
+
+	// 1. Save the superpower for each person in a variable.
+
+	// 2. Ask the user to enter a name. Store their answer in a variable.
+
+	// 3. Show the superpower in a pop-up, depending on the name entered.
+
 
